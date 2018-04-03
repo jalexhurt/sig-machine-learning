d4e9ed2e60aae8488b06ee05a437c608456f93df
James Hurt <jah3xc@mail.missouri.edu>
2018-04-03 14:23:25 -0500

    Update REPO

---
+++ kmeans.py
@@ -0,0 +1,33 @@
+# import modules
+import numpy as np
+from sklearn.cluster import KMeans
+from matplotlib import pyplot as plt
+
+## data setup
+data = np.loadtxt("six_class.csv", delimiter=',')
+
+## K MEANS
+clustering = KMeans(n_clusters=6)
+clustering.fit(data)
+
+## get results
+labels = clustering.labels_
+clusters = clustering.cluster_centers_
+
+## graph
+colors = ["blue", "red", "orange", "yellow", "green", "brown"]
+for pt, cluster in zip(data, labels):
+    i, j = pt[0], pt[1]
+    plt.scatter(i, j, color=colors[cluster % len(colors)], alpha=.5)
+
+for n, pt in enumerate(clusters):
+    i, j = pt[0], pt[1]
+    plt.scatter(
+        i,
+        j,
+        color=colors[n % len(colors)],
+        marker="X",
+        s=100,
+        edgecolor='black',
+        linewidth='3')
+plt.show()
